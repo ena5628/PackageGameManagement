abcbf2f8116e78f04c482f66579fa024fd32e485
ena256 <perfect.24ni.hakuto@gmail.com>
2026-06-29 09:26:03 +0900

    editcard.js:sessionStorageからデータを取得する処理を追加

--- public/editcard.js
+++ public/editcard.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
-
+// sessionStorageからデータを取得する処理
 const getSessionStorageData = () =>{
     const gameData = JSON.parse(sessionStorage.getItem('gameData'));  // sessionStorageからデータを取得
     const gameImageUrl = sessionStorage.getItem('gameImageUrl');  // sessionStorageから画像のURLを取得
