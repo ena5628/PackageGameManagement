cc0d5600e1ea1d15a2400502004a5ea05e2b969d
ena256 <perfect.24ni.hakuto@gmail.com>
2026-06-29 13:33:55 +0900

    editcard.js:格納した配列データをinputタグのvalueに順に挿入

--- public/editcard.js
+++ public/editcard.js
@@ -5,6 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
     // 取得できたかチェック
     if (gameData && gameImageUrl) {
         inputGameData(gameData, gameImageUrl);  // ゲームカードの挿入処理
+        // スターの作成処理
+        createStar(gameData.star_level);
     }
     else{
         console.log('SessionStorageにデータが存在しません。');
@@ -28,39 +30,50 @@ const inputGameData = (gameData, gameImageUrl) => {
     const gameKey = ['game_title', 'platform','play_date','play_status','play_time_minutes','review','star_level'];  
     
     // gameDataの値を取得
-    const gameDataArr = gameKey.map(key => {
+    const gameDataArr = [];
+    
+    // gameDataの値を配列に挿入
+    gameKey.forEach(key => {
         
         const value = gameData[key];
         
         if(key === 'play_date'){
-            return value.split('T')[0];  // 日付の形式を整える
+            gameDataArr.push(value.split('T')[0]);  // 日付の形式を整える
+            return;
         }
 
         if(key === 'play_time_minutes'){
             const hour = Math.floor(value / 60);
             const minutes = value % 60;
 
-            return {hour, minutes};  // プレイ時間の形式を整える
+            gameDataArr.push(hour);  // 時間を配列に挿入
+            gameDataArr.push(minutes);
+            return;
         }
 
-        return value;
+        gameDataArr.push(value);
     });
 
     console.log('ゲームデータの値:', gameDataArr);
 
     // // 画面に値を挿入
-    //const gameImageElement = document.getElementById('GameImage');  // ゲーム画像の要素を取得
+    const gameImageElement = document.getElementById('GameImage');  // ゲーム画像の要素を取得
 
-    // gameImageElement.src = gameImageUrl;  // ゲーム画像のURLを挿入
+    gameImageElement.src = gameImageUrl;  // ゲーム画像のURLを挿入
 
-    // const inputGameData = document.querySelectorAll('input');
-    // console.log('inputGameData:', inputGameData);
+    const inputGameData = document.querySelectorAll('input, select, textarea,hidden');  // input, select, textarea, hiddenの要素を取得
+    console.log('inputGameData:', inputGameData);
 
-    // // ゲームデータをフォームに挿入
-    // for(let i = 0; i < gameKey.length; i++){
-    //     if(inputGameData[i]){
-    //         inputGameData[i].value = gameData[gameKey[i]];  // ゲームデータをフォームに挿入
-    //     }
-    // }
+    let counter = 0;  // カウンターを初期化
+    // ゲームデータをフォームに挿入
+    for(let i = 0; i < inputGameData.length; i++){
+        if(inputGameData[i].type === 'file' || inputGameData[i].type === 'submit'){
+            continue;  // ファイル入力はスキップ
+        }
+
+        inputGameData[i].value = gameDataArr[counter++];  // ゲームデータをフォームに挿入
+
+    }
 
 };
+
